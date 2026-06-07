fddc71b7ea7e62064fa11fa7a3fa4bd25e829e78
Rishabh Thakur <rishabhthakur160906@gmail.com>
2026-06-08 00:40:34 +0530

    This is my Day_2 Assignment

---
+++ Day_2/Question_5.java
@@ -0,0 +1,21 @@
+// Code to calculate the sum of digits of a given number
+import java.util.*;
+public class Question_5 {
+    public static void main(String[] args)
+    {
+        int n ;
+        Scanner Sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        n = Sc.nextInt();
+        int n1=n;
+        int sum =0;
+        int r =0;
+        while(n>0)
+        {
+            r = n%10;
+            sum = sum +r;
+            n =n/10;
+        }
+        System.out.println("The sum of digits of "+n1+" is "+sum);
+    }
+}
