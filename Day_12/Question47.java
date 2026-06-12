a4d377e1adfaa4d0400e1ddf427972d285485be5
Rishabh Thakur <rishabhthakur160906@gmail.com>
2026-06-13 01:59:01 +0530

    This is my Day_12 Assignment

---
+++ Day_12/Question47.java
@@ -0,0 +1,33 @@
+// Code to write fuction for fibonacci series.
+import java.util.*;
+public class Question47 {
+     public static void main(String[] args)
+      {
+            int n ;
+            Scanner Sc = new Scanner(System.in);
+            System.out.print("Enter the number : ");
+            n = Sc.nextInt();
+            int fibo =fibonacci(n);
+            System.out.print(+n+"th Fibonacci term is "+fibo); 
+       }
+ public static int fibonacci(int x)
+ { 
+        if (x == 0)
+            return 0;
+        if (x == 1)
+            return 1;
+
+        int t1 = 0;
+        int t2 = 1;
+        int nextterm = 0;
+
+        for (int i=2; i<=x;i++) 
+            {
+            nextterm = t1+t2;
+            t1=t2;
+            t2=nextterm;
+        }
+
+        return nextterm;
+    }
+}
