7002ac9d960ae1d538be4e1317e1a4ed2edfcd22
Rishabh Thakur <rishabhthakur160906@gmail.com>
2026-06-21 00:45:13 +0530

    This is my Day_20 Assignment

---
+++ Day_20/Question_79.java
@@ -0,0 +1,32 @@
+// Code to find row wise sum.
+import java.util.*;
+public class Question_79 {
+    public static void main(String[] args) 
+        {
+           int r1;
+           int c1;
+           Scanner sc = new Scanner(System.in);
+           System.out.print("Enter the row of 1st matrix : ");
+           r1 = sc.nextInt(); 
+           System.out.print("Enter the column of 1st matrices : ");
+           c1 = sc.nextInt();
+           int[][] mat1 = new int[r1][c1];
+           for(int i =0;i<r1;i++)
+            {
+            for(int j =0;j<c1;j++)
+            {
+            System.out.print("Enter mat1["+i+"]["+j+"] : ");
+            mat1[i][j] = sc.nextInt();
+           }
+           }
+           for(int i =0;i<r1;i++)
+           {
+             int sum =0;
+              for(int j =0;j<c1;j++)
+              {
+                sum += mat1[i][j];
+              }
+              System.out.println("sum"+(i+1)+" is "+sum);
+           }
+}
+}
